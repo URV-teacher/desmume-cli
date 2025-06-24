7d11ba4680346a3d9696435056501dfeaf1a3b9d
AleixMT <aleixaretra@gmail.com>
2025-06-25 00:42:14 +0200

    ADDED: desmume cli functional in Docker

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM ubuntu:24.04
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+# Required packages
+RUN apt-get update && apt-get install -y \
+    build-essential  \
+    meson \
+    gcc \
+    make \
+    gettext \
+    intltool \
+    ninja-build \
+    git \
+    libsdl2-dev \
+    libpcap-dev \
+    libgtk-3-dev \
+    libopenal-dev \
+    libsoundtouch-dev \
+    libagg-dev \
+    libosmesa6-dev \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+#     git checkout 7c7fd242c221a8f722d39f9df5cdf40789cb31e3 && \
+# Clone and build DeSmuME CLI
+RUN git clone https://github.com/TASEmulators/desmume /desmume && \
+    cd /desmume && \
+    cd /desmume/desmume/src/frontend/posix && \
+    meson build -Dfrontend-cli=true --buildtype=release && \
+    mkdir -p /desmume/desmume/src/frontend/posix/build && \
+    ninja -C /desmume/desmume/src/frontend/posix/build
+
---
+++ test/test.sh
@@ -0,0 +1,36 @@
+
+export ROOT="$(cd "$(dirname "$(realpath "$0")")/.." &>/dev/null && pwd)"
+
+mount "${ROOT}/fs/fat.img" "${ROOT}/fs/mountpoint"
+
+if [ -f "${ROOT}/fs/mountpoint/out.txt" ]; then
+  echo "out.txt detected, removing"
+  rm "${ROOT}/fs/mountpoint/out.txt"
+  umount "${ROOT}/fs/mountpoint"
+  mount "${ROOT}/fs/fat.img" "${ROOT}/fs/mountpoint"
+  [ -f "${ROOT}/fs/mountpoint/out.txt" ] && echo "The file is present after deleting, something is wrong" && exit 1
+fi
+
+umount "${ROOT}/fs/mountpoint"
+{
+  cd "${ROOT}"
+  BUILDKIT_PROGRESS=plain docker compose up -d
+}
+
+mount "${ROOT}/fs/fat.img" "${ROOT}/fs/mountpoint"
+while [ ! -f "${ROOT}/fs/mountpoint/out.txt" ]; do
+  umount "${ROOT}/fs/mountpoint"
+  echo "out.txt still not detected"
+  sleep 1
+  mount "${ROOT}/fs/fat.img" "${ROOT}/fs/mountpoint"
+done
+
+echo "out.txt detected, test passed"
+rm "${ROOT}/fs/mountpoint/out.txt"
+umount "${ROOT}/fs/mountpoint"
+exit 0
+
+
+
+
+
